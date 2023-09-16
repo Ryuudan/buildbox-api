9fe03da4c5a54ff7c609477f09930b738e477b62
pyakz <markvergelbanguis@gmail.com>
2023-09-17 00:32:13 +0800

    chore: run formatter

--- internal/projects/projects.go
+++ internal/projects/projects.go
@@ -11,11 +11,11 @@ var (
 // Define package-level constants or configuration settings here.
 )
 
-	func Initialize(client *mongo.Client, router chi.Router) {
-		println("-------------- PROJECTS MODULE INITIALIZED --------------")
-		router.Get("/projects", func(w http.ResponseWriter, _ *http.Request) {
-			// Write a string response to the client.
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("This is a simple endpoint for listing projects"))
-		})
-	}
+func Initialize(client *mongo.Client, router chi.Router) {
+	println("-------------- PROJECTS MODULE INITIALIZED --------------")
+	router.Get("/projects", func(w http.ResponseWriter, _ *http.Request) {
+		// Write a string response to the client.
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("This is a simple endpoint for listing projects"))
+	})
+}
