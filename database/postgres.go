212b1b511eb05092af285b8d29aba5f4b94ea35b
pyakz <markvergelbanguis@gmail.com>
2023-09-23 20:17:56 +0800

    chore: db

---
+++ database/postgres.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Pyakz/buildbox-api/ent/generated"
+	_ "github.com/lib/pq" // Import the pq driver
+)
+
+func PostgresConnect() (*generated.Client, error) {
+
+	client, err := generated.Open("postgres", os.Getenv("POSTGRES_CONNECTION_STRING"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// Successfully connected to the database
+	log.Printf("Sucessfully connected to the database!")
+
+	return client, nil
+}
